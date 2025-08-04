b521e73577a4ecc958c631525c7c593363f51959
Andrea C <and.campisi@gmail.com>
2025-08-04 15:53:28 +0200

    sample qt app

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 
-#include <iostream>
+#include <QApplication>
+#include <QPushButton>
+#include <QWidget>
+#include <QVBoxLayout>
 
-int main()
-{
-    std::cout << "Hello world" << std::endl;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("Simple Qt6 App");
+
+    QPushButton *button = new QPushButton("Click Me");
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->addWidget(button);
+    window.setLayout(layout);
+
+    window.resize(300, 100);
+    window.show();
+
+    return app.exec();
 }
-
